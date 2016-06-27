4df8648e3070addc39dc4b09e39dab48e794acce
satyaki3794 <satyaki3794@gmail.com>
2016-06-27 18:29:01 +0530

    Added static priority scheduler

--- src/plugin/loaded_plugin.cpp
+++ src/plugin/loaded_plugin.cpp
@@ -7,6 +7,8 @@
 #include "local_priority_queue_scheduler_plugin_factory.hpp"
 #include "local_queue_scheduler.hpp"
 #include "local_queue_scheduler_plugin_factory.hpp"
+#include "static_priority_queue_scheduler.hpp"
+#include "static_priority_queue_scheduler_plugin_factory.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Add this once for each module
@@ -19,3 +21,5 @@ HPX_REGISTER_LPQ_SCHEDULER_PLUGIN_FACTORY(lp_sh, local_priority_queue_scheduler)
 typedef hpx::threads::policies::local_queue_scheduler<> l_sh;
 HPX_REGISTER_LQ_SCHEDULER_PLUGIN_FACTORY(l_sh, local_queue_scheduler);
 
+typedef hpx::threads::policies::static_priority_queue_scheduler<> sp_sh;
+HPX_REGISTER_SPQ_SCHEDULER_PLUGIN_FACTORY(sp_sh, static_priority_queue_scheduler);
